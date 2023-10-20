ddc80ebafb4175d4b029e2d3dc6a9340d5a34cd6
Alex <alex.majka.xxx@gmail.com>
2023-10-20 15:20:06 +0100

    Updated some scripts and work in the CS422 tutorial2

--- CS422/modTurtle/build/catkin_generated/setup_cached.sh
+++ CS422/modTurtle/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/media/alex/AlexPortableSSD/School/4th-year/CS422/modTurtle/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/media/alex/AlexPortableSSD/School/4th-year/CS422/modTurtle/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/media/alex/AlexPortableSSD/School/4th-year/CS422/modTurtle/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/media/alex/AlexPortableSSD/School/4th-year/CS422/modTurtle/build'
+export PYTHONPATH="/media/alex/AlexPortableSSD/School/4th-year/CS422/modTurtle/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/media/alex/AlexPortableSSD/School/4th-year/CS422/modTurtle/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/media/alex/AlexPortableSSD/School/4th-year/CS422/modTurtle/src:$ROS_PACKAGE_PATH"
