fa55ec5102079a1c0d905bec932d197e22d7a6a6
Alex <alex.majka.xxx@gmail.com>
2023-10-25 17:23:54 +0100

    CS422 Assignment1 finished

--- CS422/Assignment1/src/turtle_ctrl/src/maze_solver.py
+++ CS422/Assignment1/src/turtle_ctrl/src/maze_solver.py
@@ -1,70 +1,67 @@
 #!/usr/bin/env python3
 import rospy
-
-# Imports SetPen structure to instruct turtle not to draw its path
-# through its SetPen service interface
+import time
+from turtlesim.msg import Color
 from turtlesim.srv import SetPen
-
-# Imports Twist message structure for issuing motion commands to turtle
 from geometry_msgs.msg import Twist
-import random
 
 droprate = 30
 droppacket = 0
-rotate = False
+#count to keep track of movement timing
+count = 0
 
-# Imports Color message structure for monitoring if
-# turtle is over red square
-from turtlesim.msg import Color
 
 def colour_update(msg):
     global droprate
     global droppacket
-    global rotate
+    global count
 
-    # Check if we are at the edge of the image and if so
-    # reverse and turn 90 degrees
+    #On Red reverse
     if (((msg.r == 255) and (msg.g == 0) and (msg.b == 0))):
-        #droppacket = 1
-        backup = Twist()
-        backup.linear.x = -3
-        twist_pub.publish(backup)
-        rotate = True
+        reverse = Twist()
+        reverse.linear.x = -1
+        twist_pub.publish(reverse)
+        time.sleep(2)
+        reverse.linear.x = 0
+        reverse.angular.z= 1.57
+        twist_pub.publish(reverse)
+        time.sleep(2)
         return
 
-    print('Colour: %d %d %d ' % (msg.r, msg.g, msg.b))
+    #On white move forward and every 150 count check for a wall on the right (Right Hand Rule Algorithm)
+    if ((msg.r == 255) and (msg.g == 255) and (msg.b == 255)):
+        move = Twist()
+        if (count==150):
+            move.angular.z= -1.57 
+            twist_pub.publish(move)  
+            time.sleep(2)
+            move.linear.x = 1
+            twist_pub.publish(move)
+            count=0
 
-    # Check if we are on the Green or Blue square and if so then set
-    # moving to False and come to a dead stop
-    if ((msg.r == 0) and (msg.g == 255) and (msg.b == 0)) or ((msg.r == 0) and (msg.g == 0) and (msg.b == 255)) :
+        else:
+            move.linear.x = 1
+            twist_pub.publish(move)
+            count=count+1
+        
+        return
+    
+    #On Green Deadstop
+    if ((msg.r == 0) and (msg.g == 255) and (msg.b == 0)):
+        dead_stop = Twist()
+        twist_pub.publish(dead_stop)
+        return
+    #On Blue Deadstop
+    if ((msg.r == 0) and (msg.g == 0) and (msg.b == 255)):
         dead_stop = Twist()
         twist_pub.publish(dead_stop)
         return
 
-    # Color messages are published at a high rate by turtlesim
-    # which means that if we publish Twist messages at the same
-    # rate the motion will be very erratic
-    #
-    # To avoid this we use droppacket and droprate to control the
-    # ratio of Color messages received to Twist messages published
+    print('Colour: %d %d %d ' % (msg.r, msg.g, msg.b))
+
     droppacket = (droppacket + 1)%droprate
     if (droppacket != 0):
         return
-
-    #if rotate
-    if(rotate == True):
-        rotate_motion = Twist()
-        rotate_motion.angular.z = 1.57
-        twist_pub.publish(rotate_motion)
-
-
-    #forward motion
-    next_move = Twist() # initialises to zero motion
-    next_move.linear.x = 3
-
-
-    # Publish the next_move
-    twist_pub.publish(next_move)
 
 if __name__=='__main__':
     # Initialise node
